09917145da29a8830ea35eb5d94b7bc4216bc5b5
lina3456-pop <lina.valleespadoni@gmail.com>
2026-05-12 15:19:03 +0200

    Add files via upload

---
+++ Portfolio étudiant LINA/script.js
@@ -0,0 +1,15 @@
+const elements = document.querySelectorAll(".fade");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+elements.forEach(el => observer.observe(el));
